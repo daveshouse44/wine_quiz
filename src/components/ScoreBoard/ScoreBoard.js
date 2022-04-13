73f9527726b166c30c96ab3072c317fe93e900c6
David Tierney <daveshouse44@hotmail.com>
2022-04-13 00:51:27 -0500

    added scoreboard feature, some styling

---
+++ src/components/ScoreBoard/ScoreBoard.js
@@ -0,0 +1,16 @@
+import React from "react";
+import RecordOfQuestion from "./RecordOfQuestion/RecordOfQuestion";
+import "./ScoreBoard.css";
+
+const ScoreBoard = ({ props, fn }) => {
+  return (
+    <>
+      <h1>
+        Your Score : {props.score}
+        <RecordOfQuestion {...props} />
+        <button onClick={fn}>Reset</button>
+      </h1>
+    </>
+  );
+};
+export default ScoreBoard;
